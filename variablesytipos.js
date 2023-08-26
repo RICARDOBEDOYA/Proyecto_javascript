5ae1fe290e6f0865f2624b9d0ca4d78a6e078406
RICARDOBEDOYA <ricardobedoyavelasquez0213@gmail.com>
2023-08-26 11:44:33 -0500

    agregar variables y tipos en js

---
+++ variablesytipos.js
@@ -0,0 +1,9 @@
+let productName = "Droid";
+let pricePerItem = 2000;
+
+// Change code below this line
+productName = "Repair droid";
+pricePerItem += 1500;
+
+console.log(productName); // Output: Repair droid
+console.log(pricePerItem); // Output: 3500
